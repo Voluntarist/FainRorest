dd31eac785764a883026053cdfe5a7860d8d99c1
Luke Walker <luke.t.walker@gmail.com>
2014-02-04 21:52:57 -0500

    creating log in functionality weee

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,14 @@
 Fainrorest::Application.routes.draw do
 
 
+  get "sessions/new"
+  get "sessions/create"
+  get "sessions/destroy"
   get "users/new"
   get "users/create"
   resources :products
   resources :users, :only => [:new, :create]
-
+  resources :sessions, :only => [:new, :create, :destroy]
 
 
 
